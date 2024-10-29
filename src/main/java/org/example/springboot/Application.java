4c983a03190265a83c0502efc800ac1cf3bcaa40
202030138SanghyunChoi <shshchoi@naver.com>
2024-10-29 15:32:02 +0900

    OnStudy 파일 보고 SPRING BOOT, SPTING SHOP 프로젝트에 주석 추가하기
    그리고 이름도 다시지어라 퇴근할 생각말고

--- src/main/java/org/example/springboot/Application.java
+++ src/main/java/org/example/springboot/Application.java
@@ -2,11 +2,13 @@ package org.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // CreatedDate 와 LastModifiedBy 를 사용하기 위해 EnableJpaAuditing Annotation 사용
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/org/example/springboot/controller/UserViewController.java
+++ src/main/java/org/example/springboot/controller/UserViewController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserViewController {
+    // RestController 가 아니므로 http 상태 코드 리턴이 아닌 template 파일 이름 리턴
     @GetMapping("/login")
     public String login() {
         return "login";
     }
-
+    
     @GetMapping("signup")
     public String signup() {
         return "signup";
